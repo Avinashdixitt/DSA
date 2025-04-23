5613d1d81797d0865a17a915b7a708184157de40
Avinash Dixit <154050451+Avinashdixitt@users.noreply.github.com>
2025-04-23 23:01:52 +0530

    Time: 9 ms (51.25%) | Memory: 41 MB (59.50%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -1,20 +1,20 @@
 class Solution {
 
     public int countLargestGroup(int n) {
-        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
-        int maxValue = 0;
-        for (int i = 1; i <= n; ++i) {
-            int key = 0, i0 = i;
-            while (i0 != 0) {
-                key += i0 % 10;
-                i0 /= 10;
+        int maxValue=0;
+        Map <Integer ,Integer> hashmap=new HashMap<Integer,Integer>();
+        for (int i=1;i<=n;i++){
+            int key=0,i0=i;
+            while(i0!=0){
+                key+=i0%10;
+                i0=i0/10;
             }
-            hashMap.put(key, hashMap.getOrDefault(key, 0) + 1);
-            maxValue = Math.max(maxValue, hashMap.get(key));
+            hashmap.put(key,hashmap.getOrDefault(key,0)+1);
+            maxValue=Math.max(maxValue,hashmap.get(key));
         }
-        int count = 0;
-        for (Map.Entry<Integer, Integer> kvpair : hashMap.entrySet()) {
-            if (kvpair.getValue() == maxValue) {
+        int count=0;
+        for(Map.Entry<Integer,Integer>kv:hashmap.entrySet()){
+            if (kv.getValue()==maxValue){
                 ++count;
             }
         }
