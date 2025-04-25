8d2efe588df73e64e330118690d86beb1bafa73c
Avinash Dixit <154050451+Avinashdixitt@users.noreply.github.com>
2025-04-25 18:10:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.44%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;  // Import should be here, outside the class
+
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);  // Use the correct variable name
+    }
+}
